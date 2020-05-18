88eedb0fd0ddddda0953aad9c9b02fe39c21b763
ssm1755 <mac12140321@icloud.com>
2020-05-18 21:52:03 +0900

    numpyの基礎１

---
+++ numpy.test_179.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+'''
+問題
+変数arr同士を足したものを出力して下さい。
+変数arr同士を引いたものを出力して下さい。
+変数arrの３乗を出力して下さい。
+１を変数arrで割った値を出力して下さい。
+出力はprint()関数を用いて下さい。
+'''
+
+arr = np.array([2,5,3,4,8])
+
+#arr + arr
+print(arr + arr)
+
+#arr - arr
+print(arr - arr)
+
+#arr ** 3
+print(arr ** 3)
+
+#1 / arr
+print(1 / arr)
